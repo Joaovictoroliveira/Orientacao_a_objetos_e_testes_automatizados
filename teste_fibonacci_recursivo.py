1bd70f5d1aa27e798aa3facc161b147bdcca50c0
Joao Victor <joao.gomes31@fatec.sp.gov.br>
2020-10-09 12:38:36 -0300

    Adicionando algoritmos sobre recursão

---
+++ teste_fibonacci_recursivo.py
@@ -0,0 +1,16 @@
+import fibonacci_recursivo
+import pytest
+
+@pytest.mark.parametrize("entrada, esperado", [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (6, 8),
+    (7, 13)
+])
+
+def testa_fibonacci(entrada, esperado):
+    assert(fibonacci_recursivo.fibonacci(entrada) == esperado)
