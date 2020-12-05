738d38879b5e5eeaba471caec82ef3ebdf40c7b8
Joao Victor <joao.gomes31@fatec.sp.gov.br>
2020-12-05 11:56:54 -0300

    Finalizando módulo 5 - Manipulação de arquivos e JSON

--- python_FIAP/manipula_arquivos/leitura_arquivo.py
+++ python_FIAP/manipula_arquivos/leitura_arquivo.py
@@ -1,5 +1,8 @@
 with open("teste.txt", "r") as arquivo:
     conteudo = arquivo.readlines()
+    #for linha in conteudo:
+    #    print(linha)
 
 print("Tipo de dado da variável", type(conteudo))
 print("Conteúdo do arquivo: ", conteudo)
+
