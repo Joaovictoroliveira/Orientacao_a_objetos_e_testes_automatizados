9c791187ffb434b413ada3bfc9b5dc45eb00bc49
Joao Victor <joao.gomes31@fatec.sp.gov.br>
2020-12-17 15:40:04 -0300

    Adicionando diretório de exercícios

---
+++ exercicios/par_ou_impar.py
@@ -0,0 +1,7 @@
+bino = int(input())
+cino = int(input())
+
+if (bino + cino) % 2 == 0:
+    print("Bino")
+else:
+    print("Cino")
---
+++ exercicios/positivo_negativo_ou_nulo.py
@@ -0,0 +1,8 @@
+valor = int(input())
+
+if valor > 0:
+    print("positivo")
+elif valor < 0:
+    print("negativo")
+else:
+    print("nulo")
